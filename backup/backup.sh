d2aed8ef8d37080d2655e9cf3fb5a86ad824bdb5
Boris Moers <boris@moers.ch>
2021-12-24 15:31:26 +0100

    city & build backup

--- backup/backup.sh
+++ backup/backup.sh
@@ -3,4 +3,4 @@
 export $(grep -v '^#' ../.env | xargs -0)
 
 docker run --rm -v ${PWD}:/tmp/backup mongo mongoexport --uri=$MONGODB_URL -c city --sort='{name: 1}' -o /tmp/backup/city.json
-docker run --rm -v ${PWD}:/tmp/backup mongo mongoexport --uri=$MONGODB_URL -c build --sort='{id: 1}' -o /tmp/backup/build.json
+docker run --rm -v ${PWD}:/tmp/backup mongo mongoexport --uri=$MONGODB_URL -c build --sort='{city: 1, id: 1}' -o /tmp/backup/build.json
